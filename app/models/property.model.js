8364fb4f9227251a0bfe2e09d15cea61427f4309
oiacow <you@example.com>
2022-01-26 16:19:03 -0300

    a

---
+++ app/models/property.model.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, Sequelize) => {
+    const Property = sequelize.define("properties", {
+            ownerID: {
+                type: Sequelize.INTEGER
+            },
+        heroName: {
+            type: Sequelize.STRING
+            },
+            hero_stats: {
+            type: Sequelize.JSON
+            },
+            game_token: {
+            type: Sequelize.STRING
+            },            
+            timestamps: false
+            });
+    return Property;
+    };
