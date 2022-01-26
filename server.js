cd058d750c076e1fa587660c8e75d3ba2780aeb7
oiacow <you@example.com>
2022-01-26 16:06:59 -0300

    a

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 require('./app/routes/property.routes')(app);
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
